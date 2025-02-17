e09d0f8348c97c110559babe4aebf972f30952dc
Joselito <josalbarr@alu.edu.gva.es>
2025-02-17 16:36:15 +0100

    remake

--- Next/astral-s/app/profile/page.js
+++
@@ -1,5 +0,0 @@
-export default () =>{
-    return(
-        <h1 className="text-center">Profile</h1>
-    )
-}
--- Next/astral-s/app/ui/landing.js
+++
@@ -1,5 +0,0 @@
-export default () => {
-    return (
-        <h1>Página de pillarte el tilin (landing)</h1>
-    )
-}
